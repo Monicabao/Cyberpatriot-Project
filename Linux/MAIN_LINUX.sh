b1b7e4e5a842303f08e24f7efc357656b64c65d3
Monicabao <51468772+Monicabao@users.noreply.github.com>
2022-06-15 02:32:29 -0700

    updatinggg

--- Linux/MAIN_LINUX.sh
+++ Linux/MAIN_LINUX.sh
@@ -1,16 +1,20 @@
-echo Enable Firewall
+echo "Enable Firewall"
 sudo ufw status
-echo Remove Non-Work Related Files
-find -name "*.mp3" -exec rm -rf {} \;
-find -name "*.mp4" -exec rm -rf {} \;
-echo Remove Services
+echo "Remove Non-Work Related Files"
+find -name "*.mp3" -type f -delete
+find -name "*.mp4" -type f -delete
+find /home -name '*.gif' -type f -delete
+find /home -name '*.png' -type f -delete
+find /home -name '*.jpg' -type f -delete
+find /home -name '*.jpeg' -type f -delete
+echo "Remove Services"
 sudo service --status-all
 sudo apt remove --purge samba
 sudo apt-get remove telnetd
 Sudo apt remove --purge pure-ftpd
-echo Remove Hacking Software
+echo "Remove Hacking Software"
 sudo apt remove --purge nmap
 sudo apt remove --purge wireshark
 sudo apt remove --purge netcat
-echo Update System
+echo "Update System"
 sudo apt-get update
---
+++ Linux/USERS_LINUX.sh
@@ -0,0 +1,4 @@
+#take input of the users list
+echo "Copy and paste the users list"
+read USERS
+
