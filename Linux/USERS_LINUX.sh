59a568cd1d924b572a810a920704b7579a5e132f
Monicabao <51468772+Monicabao@users.noreply.github.com>
2022-06-15 03:27:26 -0700

    Update USERS_LINUX.sh

--- Linux/USERS_LINUX.sh
+++ Linux/USERS_LINUX.sh
@@ -1,4 +1,19 @@
-#take input of the users list
-echo "Copy and paste the administrators list"
+#Decide administrators
+echo "Who are the users in administrators?"
 read ADMINISTRATORS
+for val in $ADMINISTRATORS; do
+    sudo gpasswd -a $val sudo
+done
 
+echo "Who are the authorized users?"
+read AUTHORIZEDUSERS
+for val in $AUTHORIZEDUSERS; do
+    sudo gpasswd -d $val sudo
+done
+
+#Delete unauthorizes users
+USERS=""
+for val in $a 'ipsum' $b 'sit amet' 10
+do
+ USERS+="$val"
+done
