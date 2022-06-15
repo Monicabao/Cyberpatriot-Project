6785b4c846af2d710a5c417364961d80d745ce5a
Monicabao <51468772+Monicabao@users.noreply.github.com>
2022-06-15 04:19:45 -0700

    Update USERS_LINUX.sh

--- Linux/USERS_LINUX.sh
+++ Linux/USERS_LINUX.sh
@@ -1,19 +1,30 @@
-#Decide administrators
-echo "Who are the users in administrators?"
-read ADMINISTRATORS
+'#Decide administrators
+echo "Who are the users in administrators?";read ADMINISTRATORS
 for val in $ADMINISTRATORS; do
-    sudo gpasswd -a $val sudo
+    sudo usermod -aG sudo $val
 done
 
-echo "Who are the authorized users?"
-read AUTHORIZEDUSERS
+echo "Who are the authorized users?";read AUTHORIZEDUSERS
 for val in $AUTHORIZEDUSERS; do
     sudo gpasswd -d $val sudo
 done
 
 #Delete unauthorizes users
 USERS=""
-for val in $a 'ipsum' $b 'sit amet' 10
+for val in $ADMINISTRATORS 'ipsum' $AUTHORIZEDUSERS 'sit amet' 10
 do
  USERS+="$val"
+done'
+
+#Add user function
+add_users() {
+read -p "Do you want to add users? (Y/n): " DECISION
+while [ $DECISION == "y" ];
+do 
+  read -p "What's the name of user you would like to add: " name 
+  sudo useradd $name
+  sudo mkdir /home/$name 
+  echo "User $name created"
+  read -p "add users? (Y/n): " DECISION
 done
+}
