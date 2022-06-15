456cb9f2c8315fa6cac10c4a5beb6ba5f1524be6
Monicabao <51468772+Monicabao@users.noreply.github.com>
2022-06-15 03:33:14 -0700

    added correct perms

--- Linux/MAIN_LINUX.sh
+++ Linux/MAIN_LINUX.sh
@@ -1,17 +1,17 @@
 echo "Enable Firewall"
 sudo ufw status
 echo "Remove Non-Work Related Files"
-find -name "*.mp3" -type f -delete
-find -name "*.mp4" -type f -delete
-find /home -name '*.gif' -type f -delete
-find /home -name '*.png' -type f -delete
-find /home -name '*.jpg' -type f -delete
-find /home -name '*.jpeg' -type f -delete
+sudo find -name "*.mp3" -type f -delete
+sudo find -name "*.mp4" -type f -delete
+sudo find /home -name '*.gif' -type f -delete
+sudo find /home -name '*.png' -type f -delete
+sudo find /home -name '*.jpg' -type f -delete
+sudo find /home -name '*.jpeg' -type f -delete
 echo "Remove Services"
 sudo service --status-all
 sudo apt remove --purge samba
 sudo apt-get remove telnetd
-Sudo apt remove --purge pure-ftpd
+sudo apt remove --purge pure-ftpd
 echo "Remove Hacking Software"
 sudo apt remove --purge nmap
 sudo apt remove --purge wireshark
